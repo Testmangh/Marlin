eb5045dad62b2c9419c8eb8a72c4387bb46c9e6c
Testmangh <anythingnothing82@gmail.com>
2021-02-21 13:17:10 -0300

    Placa mae

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_CREALITY_V4
+  #define MOTHERBOARD BOARD_BTT_SKR_V1_3
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
